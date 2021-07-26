09706bc82295227beaf5d4c1a188dbce42a22665
Pavel Kursenko <69045049+Poul1981@users.noreply.github.com>
2021-07-26 22:28:26 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,186 @@
+$(function () {
+    let enemy = $(".enemy");
+    let shooter = $(".shooter");
+    let missile = $(".missel");
+    let bomb = $(".bomb");
+    let shooter_bar = $(".shooter_bar");
+    let enemy_bar = $(".enemy_bar");
+    let popup = $(".popup");
+    let pop_content = $(".popup_content");
+    let closed = $(".popup_close");
+
+    ////////////////////////////////
+    let trigger = $(".popup_trigger");
+    trigger.on("click", modalWindow);
+    ////////////////////////////////
+    closed.on("click", (event) => {
+        popup.removeClass("active");
+        pop_content.removeClass("active");
+        event.preventDefault();
+    });
+
+    $(window).resize(() => {
+        distance = $(".container").height() - 50;
+        console.log(distance);
+    });
+    ////////////////////////////////
+
+    let direction = 10;//скорость движения защитника
+
+    let distance = $(".container").height() - 50; //расстояние полета ракеты/бомбы
+    let duration = distance / 2;//продолжительность полета ракеты/бомбы
+    console.log(distance);
+    let enemy_life = 200;
+    let shooter_life = 200;
+
+    //идентификатор движения
+    let move;
+    //flag for button
+    let down = false;
+
+    missile.hide();
+    bomb.hide();
+    let timerForBomb = setInterval(lanchBomb, 500);
+
+    function lanchBomb() {
+        let positionEnemy = enemy.position().left;
+        let bomb = $("<div class='bomb'></div>");
+        $(".container").prepend(bomb);
+        bomb.css("left", positionEnemy + 40);
+        bomb.animate({ top: `+=${distance}px` }, duration, "linear");
+        setTimeout(() => {
+            //определяем результат бомбардировки
+            let bomb1 = document.querySelector(".bomb");
+            let shooter1 = document.querySelector(".shooter");
+            if (testCollision(bomb1, shooter1)) {
+                //бомба попала в цель
+                // bomb.remove();
+                shooter.css({
+                    "background": "url(img/unoDamage.png) no-repeat center",
+                    "object-fit": "cover",
+                    "background-size": "contain",
+                });
+                shooter_life -= 5;
+                doDamage(shooter_bar, shooter_life);
+                setTimeout(() => {
+                    shooter.css({
+                        "background": "url(img/uno1.png) no-repeat center",
+                        "object-fit": "cover",
+                        "background-size": "contain",
+                    });
+                }, 300);
+            }
+            bomb.width(100).height(100);
+            bomb.css({
+                "background": "url(img/boom.png) no-repeat center",
+                "object-fit": "cover"
+            });
+            bomb.remove();
+        }, duration);
+    }
+
+    function doDamage(bar, life) {
+        bar.width(life);
+        if (life < 1) {
+            // alert(`${bar.attr("data-desc")} победил!`)
+            let message = bar.attr("data-desc");
+            modalWindow(message);
+        }
+    }
+
+    function modalWindow(mes) {
+        popup.addClass("active");
+        pop_content.addClass("active");
+        if (mes == "Враг") {
+            $(".popup_title").text("Поражение!")
+            $(".popup_text p").text("Защитник пал под натиском врага...")
+            shooter.hide();
+        }
+        else {
+            clearInterval(timerForBomb);
+            enemy.css("animation-play-state", "paused");
+            enemy.hide();
+        }
+    }
+
+    function testCollision(div1, div2) {
+        let i = div1.getBoundingClientRect();
+        let j = div2.getBoundingClientRect();
+        return i.top + i.height > j.top &&
+            i.left + i.width > j.left &&
+            i.bottom - i.height < j.bottom &&
+            i.right - i.width < j.right;
+    }
+
+    $("html").keydown(function (event) {
+        let pos = shooter.position().left;
+        let container_width = $(".container").width();
+
+        //button to left
+        if (event.which == "37" && pos > -10) {
+            moveLeftOreRight(-direction, pos, container_width);
+        }
+        //button to right
+        if (event.which == "39" && pos < (container_width - 80)) {
+            moveLeftOreRight(direction, pos, container_width);
+        }
+    });
+
+    //если клавиша отжата
+    $("html").keyup(function (event) {
+        let pos = shooter.position().left;
+        if (event.which == "37" || event.which == "39") {
+            down = false;
+            clearInterval(move);
+        }
+        if (event.which == "32") {
+            lanchMissile(pos);
+        }
+    });
+
+    function moveLeftOreRight(direction, pos, width) {
+        if (down) return;
+        down = true;
+        // clearInterval(move);
+        move = setInterval(() => {
+            shooter.css("left", pos);
+            pos += direction;
+            if (pos < -20 || pos > (width - 80)) {
+                clearInterval(move);
+            }
+        }, 40);
+    }
+    //запуск ракеты
+    function lanchMissile(pos) {
+        let mis = $("<div class='missel'></div>");
+        $(".container").append(mis);
+        mis.css("left", pos + 45);
+        mis.animate({ top: `-=${distance}px` }, duration, "linear");
+        // mis.animate({ top: "-=900px" }, { duration: 800, easing: "linear" });
+        setTimeout(() => {
+            //результат попадания
+            let missel1 = document.querySelectorAll(".missel");
+            let enemy1 = document.querySelector(".enemy");
+            if (testCollision(missel1[1], enemy1)) {
+                //если ракета попала во врага
+                mis.remove();
+                enemy.css({
+                    "background": "url(img/catUfoDamage.png) no-repeat center",
+                    "object-fit": "cover",
+                    "background-size": "contain",
+                });
+                enemy_life -= 5;
+                doDamage(enemy_bar, enemy_life);
+                setTimeout(() => {
+                    enemy.css({
+                        "background": "url(img/catUfo.png) no-repeat center",
+                        "object-fit": "cover",
+                        "background-size": "contain",
+                    });
+                }, 300);
+            }
+            mis.remove()
+        }, duration);//430 через какое время ракета самоуничтожается
+    }
+})
+
